cc7ca1206513c823e79f13b3f810b7db8c57ca4b
Alex <alexander.s.carney.ug@dartmouth.edu>
2021-09-11 16:44:43 -0400

    update dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,8 @@ COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/dist ./dist
 
+COPY --from=builder /app/views ./views       
+COPY --from=builder /app/public ./public
+
 EXPOSE 3000
 CMD [ "npm", "run", "start:prod" ]
--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,9 @@ declare const module: any
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication> (
+    AppModule
+    );
 
   // Validation
   app.useGlobalPipes(new ValidationPipe());
